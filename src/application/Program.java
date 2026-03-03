9f228616b3b346b22879e006c98d4b896d09f858
fnxis <guiclash4759@gmail.com>
2026-03-03 18:03:25 -0300

    Create DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +16,9 @@ public class Program {
         System.out.println(department);
         Seller seller = new Seller(1,"Guilherme","guiclash4759@gmail.com",new Date(),2000D,department);
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
 
     }
 
